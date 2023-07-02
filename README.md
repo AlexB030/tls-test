3f94796e7ae7dcc0ad0bea2ed59c9b2c3f84d33b
Alexander Bajic <bajic@me.com>
2023-07-02 18:25:54 +0200

    Extended tests

--- README.md
+++ README.md
@@ -7,7 +7,9 @@ The shell script in this repository performs different tests against chosen serv
 The given script assumes the following packages to be installed:
 * tee
 * curl (tested: 7.81.0, 7.88.1)
-* suported SSL library, e.g. OpenSSL (tested: 3.0.2) or LibreSSL (tested: 3.3.6)
+* OpenSSL (tested: 3.0.2, 3.1.1)
+
+__Note:__ LibreSSL is not an adequate substitute for OpenSSL with regard to its behaviour when using it as a tls-client (s_client option) to download certificates! It will not neccessarily run into errors but produce varying output when fetching certificates from servers with more than one virtual host. Not sure if this is the server's fault (tested with nginx) or LibreSSL's, yet using openssl produces output as expected.
 
 The script should be fine with the standard shell (sh). Yet, it has only been tested with bash and zsh.
 
@@ -18,6 +20,10 @@ The script should be fine with the standard shell (sh). Yet, it has only been te
 | 1 | TLS 1.1 | This test checks whether the targeted TLS server supports TLS version 1.1. | TLS 1.1 connections must be declined by the server for this test to succeed. |
 | 2 | TLS 1.2 | This test checks whether the targeted TLS server supports TLS version 1.2. | TLS 1.2 connections must be accepted by the server for this test to succeed. |
 | 3 | TLS 1.3 | This test checks whether the targeted TLS server supports TLS version 1.3. | TLS 1.3 is neither required, nor forbidden. Yet, support for TLS 1.3 may be nice to know. |
+| 4 | TLS 1.2 ECC-1 | This test checks whether the targeted TLS server supports TLS version 1.2 using the cipher suite TLS_ECDHE_ECDSA_WITH_AES_128_GCM_SHA256 | Cipher suite must be accepted by the server for this test to succeed. |
+| 5 | TLS 1.2 ECC-2 | This test checks whether the targeted TLS server supports TLS version 1.2 using the cipher suite TLS_ECDHE_ECDSA_WITH_AES_256_GCM_SHA384 | Cipher suite must be accepted by the server for this test to succeed. |
+| 6 | x509 fetch | Ability to fetch the x509 certificate is not a test but a pre-condition for subsequent tests for parameters | Download must be successfull for subsequent checks |
+| 7 | x509 pubkey algorithm | Checks the x.509 certificate's public key algorithm by grepping for the respective expression when printing the certificate using openssl (ToDO: This should be turned into a check for OID in the ASN.1 representation) | Expression 'Public Key Algorithm: id-ecPublicKey' must be found exactly once |
 
 ## Usage
 Currently, the behaviour of this script cannot be altered through passing parameters as arguments or dedicated config files. The only argument that the script expects is the domain name of the server to be tested.
--- tls-test.sh
+++ tls-test.sh
@@ -1,5 +1,7 @@
 # list of recognised applications
-# 11 - curl
+# 11 - curl tls-client
+# 21 - openssl tls-client
+# 31 - grep
 
 
 # colour definitions
@@ -9,8 +11,49 @@ GREEN='\033[0;32m';
 YELLOW='\033[0;33m';
 NOCOLOR='\033[0m';
 
+GENERALERROR=0;
 
 # functions
+grep_handler()
+{
+    echo "Grep return value handler";
+    echo "returned $1";
+    echo "sign: $2";
+    GENERALERROR=0;
+
+    # start return value handling using nested IFs as per category
+    if [ $1 -eq 0 ]
+    then
+        echo "found expression...";
+        if [ $3 -eq $4 ]
+        then
+            echo "in desired number";
+        else
+            echo "NOT in desired number";
+            GENERALERROR=1;
+        fi
+    else
+        echo "could not find expression";
+    fi
+}
+
+openssl_handler()
+{
+    echo "Openssl return value handler";
+    echo "returned $1";
+    echo "sign: $2";
+    GENERALERROR=0;
+
+    # start return value handling using nested IFs as per category
+    if [ $1 -eq 0 ]
+    then
+        echo "certificate fetched";
+    else
+        echo "could not fetch certificate";
+        GENERALERROR=1;
+    fi
+}
+
 curl_handler()
 {
     echo "Curl return value handler";
@@ -29,38 +72,21 @@ curl_handler()
             echo "unsupported feature - this is an error of your environment!";
             GENERALERROR=1;
         else
-            if [ $1 -eq 0 ]
+            if [ $1 -eq 6 ]
             then
-                echo "connection established";
+                echo "bad domain";
+                GENERALERROR=1;
             else
-                if [ $1 -eq 35 ]
+                if [ $1 -eq 0 ]
                 then
-                    echo "connection declined for unsupported TLS version";
+                    echo "connection established";
                 else
-                    echo "Unexpected return value!";
-                fi
-            fi
-        fi
-    fi
-    # interpret and colour verdict
-    if [ $2 -eq 0 ]
-    then
-        echo "${BLUE}IRRELEVANT (at the moment)${NOCOLOR}";
-    else
-        if [ $GENERALERROR -eq 1 ]
-        then
-            echo "${RED}BAD${NOCOLOR}";
-        else
-            # wir sind im normalen track
-            if [ $1 -eq 0 ] && [ $2 -eq 1 ]
-            then
-                echo "${GREEN}GOOD${NOCOLOR}";
-            else
-                if [ $1 -ne 0 ] && [ $2 -eq -1 ];
-                then
-                    echo "${GREEN}GOOD${NOCOLOR}";
-                else
-                    echo "${RED}BAD${NOCOLOR}";
+                    if [ $1 -eq 35 ]
+                    then
+                        echo "connection declined for unsupported TLS version or cipher suite";
+                    else
+                        echo "Unexpected return value!";
+                    fi
                 fi
             fi
         fi
@@ -71,19 +97,51 @@ curl_handler()
 # this function expects three arguments: 
 # arg1 : return value of previous command 
 # arg2 : application id (from list of recognised applications)
-# arg3 : sign (-1 - negativ; 0 - neutral; 1 - positive)
+# arg3 : sign (-1 - negative; 0 - neutral; 1 - positive)
+# arg<n> : test specific purpose
 return_value_handler()
 {
     if [ $2 -eq 11 ]
     then
         curl_handler $1 $3;
     else
-        echo "Unknown application to handle return value!";
-        echo "returned $1";
-        echo "app: $2";
-        echo "sign: $3";
+        if [ $2 -eq 21 ]
+        then
+            openssl_handler $1 $3;
+        else
+            if [ $2 -eq 31 ]
+            then
+                grep_handler $1 $3 $4 $5;
+            else
+                echo "Unknown application to handle return value!";
+                echo "returned $1";
+                echo "app: $2";
+                echo "sign: $3";
+            fi
+        fi
     fi
-    
+    # interpret and colour verdict
+    if [ $3 -eq 0 ]
+    then
+        echo "${BLUE}IRRELEVANT (at the moment)${NOCOLOR}";
+    else
+        if [ $GENERALERROR -eq 1 ]
+        then
+            echo "${RED}BAD${NOCOLOR}";
+        else
+            if [ $1 -eq 0 ] && [ $3 -eq 1 ]
+            then
+                echo "${GREEN}GOOD${NOCOLOR}";
+            else
+                if [ $1 -ne 0 ] && [ $3 -eq -1 ];
+                then
+                    echo "${GREEN}GOOD${NOCOLOR}";
+                else
+                    echo "${RED}BAD${NOCOLOR}";
+                fi
+            fi
+        fi
+    fi
 }
 
 
@@ -98,14 +156,32 @@ echo "TLS Test Automator" | tee -a $LOG;
 
 echo "Domain to be tested: $1" | tee -a $LOG;
 
-echo "\nTesting TLS 1.1" | tee -a $LOG;
+echo "\n01. Testing TLS 1.1" | tee -a $LOG;
 curl https://$DOMAIN --verbose --tlsv1.1 --tls-max 1.1 >>$LOG 2>>$LOG;
 return_value_handler $? 11 -1
 
-echo "\nTesting TLS 1.2" | tee -a $LOG;
+echo "\n02. Testing TLS 1.2" | tee -a $LOG;
 curl https://$DOMAIN --verbose --tlsv1.2 --tls-max 1.2 >>$LOG 2>>$LOG;
 return_value_handler $? 11 1
 
-echo "\nTesting TLS 1.3" | tee -a $LOG;
+echo "\n03. Testing TLS 1.3" | tee -a $LOG;
 curl https://$DOMAIN --verbose --tlsv1.3 --tls-max 1.3 >>$LOG 2>>$LOG;
 return_value_handler $? 11 0
+
+echo "\n04. Testing TLS 1.2 using TLS_ECDHE_ECDSA_WITH_AES_128_GCM_SHA256" | tee -a $LOG;
+curl https://$DOMAIN --verbose --tlsv1.2 --tls-max 1.2 --ciphers ECDHE-ECDSA-AES128-GCM-SHA256 >>$LOG 2>>$LOG;
+return_value_handler $? 11 1
+
+echo "\n05. Testing TLS 1.2 using TLS_ECDHE_ECDSA_WITH_AES_256_GCM_SHA384" | tee -a $LOG;
+curl https://$DOMAIN --verbose --tlsv1.2 --tls-max 1.2 --ciphers ECDHE-ECDSA-AES256-GCM-SHA384 >>$LOG 2>>$LOG;
+return_value_handler $? 11 1
+
+echo "\n06. Fetching x509 certificate" | tee -a $LOG;
+CERTIFICATE=$(openssl s_client -showcerts -connect $DOMAIN:443 </dev/null 2>>$LOG | openssl x509 -noout -text);
+return_value_handler $? 21 1
+echo "$CERTIFICATE" >>$LOG;
+
+# this should be done using the OID
+echo "\n07. Checking public key algorithm of x509 certificate" | tee -a $LOG;
+OCCURENCES=$(echo "$CERTIFICATE" | grep -c 'Public Key Algorithm: id-ecPublicKey');
+return_value_handler $? 31 1 $OCCURENCES 1
